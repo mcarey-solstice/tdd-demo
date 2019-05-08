93d3efeb5464ed442866436e377a9e564bacb3cf
Mike Carey <mcarey@solstice.com>
2019-05-08 08:48:42 -0500

    Adds code for demo

--- src/main/java/com/demo/tdd/PalindromeService.java
+++ src/main/java/com/demo/tdd/PalindromeService.java
@@ -2,6 +2,17 @@ package com.demo.tdd;
 
 public class PalindromeService {
 
-//    public boolean isPalindrome(String subject) {}
+    public boolean isPalindrome(String subject) {
+        return _isPalindrome(subject.replaceAll("[^a-zA-Z]", "").toLowerCase());
+    }
+
+    public boolean _isPalindrome(String subject) {
+        if (subject.length() <= 1) {
+            return true;
+        }
+
+        return subject.charAt(0) == subject.charAt(subject.length() - 1)
+                && isPalindrome(subject.substring(1, subject.length() - 1));
+    }
 
 }
--- src/test/java/com/demo/tdd/PalindromeServiceTest.java
+++ src/test/java/com/demo/tdd/PalindromeServiceTest.java
@@ -8,4 +8,51 @@ public class PalindromeServiceTest {
 
     PalindromeService service = new PalindromeService();
 
+    @Test
+    public void emptyStringIsPalindrome() {
+        assertTrue(service.isPalindrome(""));
+    }
+
+    @Test
+    public void singleCharacterIsPalindrome() {
+        assertTrue(service.isPalindrome("a"));
+    }
+
+    @Test
+    public void twoCharactersThatAreNotPalindrome() {
+        assertFalse(service.isPalindrome("ab"));
+    }
+
+    @Test
+    public void twoCharactersThatArePalindrome() {
+        assertTrue(service.isPalindrome("aa"));
+    }
+
+    @Test
+    public void threeCharactersThatArePalindrome() {
+        assertTrue(service.isPalindrome("aba"));
+    }
+
+    @Test
+    public void threeCharactersThatAreNotPalindrome() {
+        assertFalse(service.isPalindrome("abca"));
+    }
+
+    @Test
+    public void realWords() {
+        assertTrue(service.isPalindrome("level"));
+        assertTrue(service.isPalindrome("racecar"));
+    }
+
+    @Test
+    public void sentencesCanBePalindromes() {
+        assertTrue(service.isPalindrome("a car, a man, a maraca."));
+        assertTrue(service.isPalindrome("A car, a man, a maraca."));
+    }
+
+    @Test
+    public void sentencesCanBeNotPalindromes() {
+        assertFalse(service.isPalindrome("a car, a man, a bike, and a maraca."));
+    }
+
 }
